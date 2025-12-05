2c7946f968e05f2768727abdaba5fccad7d8b2fa
Eshmurodov Jasur <jasureshmurod78@gmail.com>
2025-12-05 16:09:41 +0500

    function Componetnga O'tkazdim

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,78 +1,76 @@
-import React from 'react'
-import MovieService from '../../services/movie-service'
-import Error from '../error/error'
-import Spinner from '../spinner/spinner'
-import "./hero.scss"
+import { useEffect, useState } from 'react';
+import MovieService from '../../services/movie-service';
+import Error from '../error/error';
+import Spinner from '../spinner/spinner';
+import './hero.scss';
 
-class Hero extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			movie: {},
-			loading: true,
-			error: false
-		}
-		this.movieService = new MovieService()
-	}
+const Hero = () => {
+	const [movie, setMovie] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
 
-	componentDidMount() {
-		this.updateMovie()
-	}
+	const movieService = new MovieService();
 
-	updateMovie = () => {
-		this.setState({loading: true})
+	useEffect(() => {
+		updateMovie();
+	}, []);
 
-		this.movieService.getRandomMovie()
-			.then(res => this.setState({movie: res}))
-			.catch(() => this.setState({error: true}))
-			.finally(() => this.setState({loading: false}))
-	}
+	const updateMovie = () => {
+		setLoading(true);
 
-	render() {
-		const {movie, loading, error} = this.state
+		movieService
+			.getRandomMovie()
+			.then(res => setMovie(res))
+			.catch(() => setError(true))
+			.finally(() => setLoading(false));
+	};
 
-		const errorContent = error ? <Error /> : null
-		const loadingContent = loading ? <Spinner /> : null
-		const content = !(error || loading) ? <Content movie={movie} /> : null
+	const errorContent = error ? <Error /> : null;
+	const loadingContent = loading ? <Spinner /> : null;
+	const content = !(error || loading) ? <Content movie={movie} /> : null;
 
-		return (
-			<div className='hero'>
-				<div className='hero__info'>
-					<h2>FIND MOVIES</h2>
-					<h1>TV shows and more</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sunt necessitatibus veritatis labore provident similique neque praesentium debitis maiores. Nihil consectetur, veniam labore magnam ab similique optio perferendis error earum.
-					</p>
-					<div>
-						<button className='btn btn-primary'>Details</button>
-						<button className='btn btn-secondary' onClick={this.updateMovie}>Random Movie</button>
-					</div>
-				</div>
-				<div className='hero__movie'>
-					{errorContent}
-					{loadingContent}
-					{content}
+	return (
+		<div className='hero'>
+			<div className='hero__info'>
+				<h2>FIND MOVIES</h2>
+				<h1>TV shows and more</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat sunt necessitatibus
+					veritatis labore provident similique neque praesentium debitis maiores. Nihil consectetur,
+					veniam labore magnam ab similique optio perferendis error earum.
+				</p>
+				<div>
+					<button className='btn btn-primary'>Details</button>
+					<button className='btn btn-secondary' onClick={updateMovie}>
+						Random Movie
+					</button>
 				</div>
 			</div>
-		)
-	}
-}
+			<div className='hero__movie'>
+				{errorContent}
+				{loadingContent}
+				{content}
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
 
-const Content = ({movie}) => {
+const Content = ({ movie }) => {
 	return (
 		<>
 			<img src={movie.backdrop_path} alt='img' />
-	
+
 			<div className='hero__movie-descr'>
-				<h2>{movie.name}</h2>	
-				<p>{movie.description && movie.description.length >= 250 
-					? `${movie.description.slice(0,250)}...` 
-					: movie.description}
+				<h2>{movie.name}</h2>
+				<p>
+					{movie.description && movie.description.length >= 250
+						? `${movie.description.slice(0, 250)}...`
+						: movie.description}
 				</p>
 				<button className='btn btn-primary'>Details</button>
 			</div>
 		</>
-	)
-}
+	);
+};
